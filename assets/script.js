e6835b486b1e667d96d53e5dc11f390d53254955
Shannen Bendeck <shay@shannens-mbp.myfiosgateway.com>
2022-05-24 00:32:15 -0400

    java

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+var url =
+`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+
+var apiKey= '30a2d391f68de9cefe133a3ae309f3b2';
+
+var searchBtn =document.querySelector("#searchBtn")
+var inputEl =document.querySelector("#search-input")
+
+searchBtn.onclick =function(event) {
+    var value = url +'&units=' + value;
+
+    fetch()
+
+
+}
